f3eed3997cb1815ef0cdb7963674f83a3f6f0e7d
wagnermarques <wagnerdocri@gmail.com>
2019-10-26 03:12:16 -0300

    agora sim, todo o projeto esta adicionado, inclusive a atividade que envia email

---
+++ start_bonita_for_this_workspace.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+bonitaExec=/home/wagner/wagnerdocri@gmail.com3/progsativos/BonitaStudioCommunity-7.9.3/BonitaStudioCommunity
+workspaceDir=/home/wagner/wagnerdocri@gmail.com3/envs/env-dev/sources/SIN5009WithBonitasoft
+$bonitaExec -data $workspaceDir
